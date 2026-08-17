5d5fcaed5efe3c653d1fab89b2078b0926430feb
Salvatore Dello Iacono <delloiaconos@gmail.com>
2026-08-17 15:37:09 +0200

    Updated applications

--- apps/estimator/main.cpp
+++ apps/estimator/main.cpp
@@ -1,3 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+/**
+ * @file
+ * @brief Application for running the battery estimator.
+ *
+ * @details
+ * Part of the BEAST project:
+ * Battery Estimation Architecture and Simulation Toolkit.
+ *
+ * BEAST is a framework for battery modeling, simulation, and state/parameter
+ * estimation.
+ *
+ * @author Salvatore Dello Iacono
+ * @date 2014
+ *
+ * @par Project
+ * BEAST - Battery Estimation Architecture and Simulation Toolkit
+ *
+ * @par Repository
+ * <REPOSITORY_URL>
+ *
+ * @copyright
+ * Copyright (c) 2026 Salvatore Dello Iacono.
+ *
+ * @license
+ * GNU General Public License v3.0.
+ */
+
+
 #include <beast/runtime/estimator_runner.h>
 
 #include <cstdio>
--- apps/model-info/main.cpp
+++ apps/model-info/main.cpp
@@ -1,3 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+/**
+ * @file
+ * @brief Application for displaying models information.
+ *
+ * @details
+ * Part of the BEAST project:
+ * Battery Estimation Architecture and Simulation Toolkit.
+ *
+ * BEAST is a framework for battery modeling, simulation, and state/parameter
+ * estimation.
+ *
+ * @author Salvatore Dello Iacono
+ * @date 2014
+ *
+ * @par Project
+ * BEAST - Battery Estimation Architecture and Simulation Toolkit
+ *
+ * @par Repository
+ * <REPOSITORY_URL>
+ *
+ * @copyright
+ * Copyright (c) 2026 Salvatore Dello Iacono.
+ *
+ * @license
+ * GNU General Public License v3.0.
+ */
+
+
 #include <beast/runtime/factories.h>
 
 #include <iostream>
@@ -6,12 +36,12 @@ int main()
 {
     std::cout << "Supported cell models:\n";
     for (const auto& name : beast::supported_cell_models()) {
-        std::cout << "  " << name << '\n';
+        std::cout << " - " << name << '\n';
     }
 
     std::cout << "\nSupported estimators:\n";
     for (const auto& name : beast::supported_estimators()) {
-        std::cout << "  " << name << '\n';
+        std::cout << " - " << name << '\n';
     }
     return 0;
 }
